a2e91182f0b62e8f11904fa8758c124f859cd10d
santiagoposadag <santiago90dell@gmail.com>
2021-12-22 15:35:16 -0500

    initialicing project

---
+++ src/main/java/com/santiagoposadag/cs50/receiverconsumer/MicroserviceReceiverConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.santiagoposadag.cs50.receiverconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceReceiverConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceReceiverConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santiagoposadag/cs50/receiverconsumer/MicroserviceReceiverConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiagoposadag.cs50.receiverconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceReceiverConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
