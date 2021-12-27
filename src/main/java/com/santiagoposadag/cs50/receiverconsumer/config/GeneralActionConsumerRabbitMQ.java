2425c4f728ac10a952727a72c8f40821fdb95e75
santiagoposadag <santiago90dell@gmail.com>
2021-12-27 00:15:27 -0500

    Finished adding the functionality of consuming the queues in rabbit

--- src/main/java/com/santiagoposadag/cs50/receiverconsumer/config/GeneralActionConsumerRabbitMQ.java
+++ src/main/java/com/santiagoposadag/cs50/receiverconsumer/config/GeneralActionConsumerRabbitMQ.java
@@ -6,6 +6,7 @@ package com.santiagoposadag.cs50.receiverconsumer.config;
 import com.santiagoposadag.cs50.receiverconsumer.usecases.RecieveFromBuyActionQueueUseCase;
 import com.santiagoposadag.cs50.receiverconsumer.usecases.RecieveFromGeneralActionQueueUseCase;
 import com.santiagoposadag.cs50.receiverconsumer.usecases.RecieveFromSellActionQueueUseCase;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -19,47 +20,20 @@ public class GeneralActionConsumerRabbitMQ {
     public static final String SELL_QUEUE = "action.sell";
     public static final String BUY_QUEUE = "action.buy";
 
-    public static final String EXCHANGE = "actions_exchange";
-    public static final String GENERAL_ROUTING_KEY = "routingKey.*";
-    public static final String SELL_ROUTING_KEY = "routingKey.sell";
-    public static final String BUY_ROUTING_KEY = "routingKey.buy";
 
-
-    @Bean
-    SimpleMessageListenerContainer containerGeneralAction(ConnectionFactory connectionFactory,
-                                             MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(GENERAL_QUEUE);
-        container.setMessageListener(listenerAdapter);
-        return container;
+    @RabbitListener(queues = GENERAL_QUEUE)
+    public void listenerOfGenralActions(String received){
+        System.out.println("Received from general actions: " + received);
     }
 
-    @Bean
-    MessageListenerAdapter listenerAdapter(RecieveFromGeneralActionQueueUseCase receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
+    @RabbitListener(queues = SELL_QUEUE)
+    public void listenerOfSellActions(String received){
+        System.out.println("Received from sell actions: " + received);
     }
 
-    @Bean
-    SimpleMessageListenerContainer containerSellAction(ConnectionFactory connectionFactory,
-                                                          MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(SELL_QUEUE);
-        container.setMessageListener(listenerAdapter);
-        return container;
+    @RabbitListener(queues = BUY_QUEUE)
+    public void listenerOfBuyActions(String received){
+        System.out.println("Received from buy actions: " + received);
     }
-
-
-    @Bean
-    SimpleMessageListenerContainer containerBuyAction(ConnectionFactory connectionFactory,
-                                                       MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(BUY_QUEUE);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
 
 }
